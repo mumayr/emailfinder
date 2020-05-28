0cfce218df211cc7fedb5b6c34d283a95b998a40
Mohammed Umayr <Mohammed.Umayr1@gmail.com>
2020-05-27 22:07:09 -0500

    first commit

---
+++ src/test/java/emailFinderTest.java
@@ -0,0 +1,89 @@
+package edu.depaul.email;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import java.io.*;
+import java.io.File;
+import java.util.Scanner;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class emailFinderTest {
+    @Test
+    @DisplayName("Testing email crawler")
+    void test() throws FileNotFoundException {
+        File file = new File("C:\\Users\\Moham\\OneDrive\\Desktop\\email-finder-master\\email-finder-master\\email.txt");
+        file.delete();
+        EmailFinder test = new EmailFinder();
+        String[] testlist = new String[1];
+        testlist[0] = "https://law.depaul.edu/Pages/default.aspx";
+        test.run(testlist);
+        int c = 0;
+        Scanner scan = new Scanner(new File("C:\\Users\\Moham\\OneDrive\\Desktop\\email-finder-master\\email-finder-master\\email.txt"));
+        while (scan.hasNextLine()) {
+            c++;
+            scan.nextLine();
+        }
+        assertEquals(12, c);
+
+    }
+
+    @Test
+    @DisplayName("Testing email crawler for 10 emails")
+    void testCertainNumberFour() throws FileNotFoundException {
+        File file = new File("C:\\Users\\Moham\\OneDrive\\Desktop\\email-finder-master\\email-finder-master\\email.txt");
+        file.delete();
+        EmailFinder test = new EmailFinder();
+        String[] testlist = new String[1];
+        testlist[0] = "C:\\Users\\Moham\\OneDrive\\Desktop\\FinalProjectSend\\EmailFinderWebsite\\MidtermProject\\Html\\midterm3.html";
+        test.run(testlist);
+        int c = 0;
+        Scanner scan = new Scanner(new File("C:\\Users\\Moham\\OneDrive\\Desktop\\email-finder-master\\email-finder-master\\email.txt"));
+        while (scan.hasNextLine()) {
+            c++;
+            scan.nextLine();
+        }
+        assertEquals(10, c);
+    }
+
+    @Test
+    @DisplayName("Testing email crawler for 12 emails")
+    void testCertainNumberTwelve() throws FileNotFoundException {
+        File file = new File("C:\\Users\\Moham\\OneDrive\\Desktop\\email-finder-master\\email-finder-master\\email.txt");
+        file.delete();
+        EmailFinder test = new EmailFinder();
+        String[] testlist = new String[1];
+        testlist[0] = "C:\\Users\\Moham\\OneDrive\\Desktop\\FinalProjectSend\\EmailFinderWebsite\\MidtermProject\\Html\\midterm2.html";
+        test.run(testlist);
+        int c = 0;
+        Scanner scan = new Scanner(new File("C:\\Users\\Moham\\OneDrive\\Desktop\\email-finder-master\\email-finder-master\\email.txt"));
+        while (scan.hasNextLine()) {
+            c++;
+            scan.nextLine();
+        }
+        assertEquals(12, c);
+    }
+
+    @Test
+    @DisplayName("Testing email crawler for no emails on no website")
+    void testEmpty() throws FileNotFoundException {
+        File file = new File("C:\\Users\\Moham\\OneDrive\\Desktop\\email-finder-master\\email-finder-master\\email.txt");
+        file.delete();
+        EmailFinder test = new EmailFinder();
+        String[] testlist = new String[1];
+        testlist[0] = "";
+        test.run(testlist);
+        int c = 0;
+        Scanner scan = new Scanner(new File("C:\\Users\\Moham\\OneDrive\\Desktop\\email-finder-master\\email-finder-master\\email.txt"));
+        while (scan.hasNextLine()) {
+            c++;
+            scan.nextLine();
+        }
+        assertEquals(0, c);
+
+    }
+}
